ca07e60a978f367f62bba6e3fcd1a6e02adbaab4
Joseph <sejokarizz@gmail.com>
2023-07-12 21:18:53 +0300

    make screen full size

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import Navbar from './navbar';
-import './App.css';
-import Footer from './footer';
+import React from "react";
+import Navbar from "./navbar";
+import "./App.css";
+import Footer from "./footer";
 
 const App = () => {
   return (
     <div className="app">
-      <Navbar/>
+      <Navbar />
       <div className="video-container">
-        <video className="background-video" autoPlay muted loop>
-          <source src="/pexels-george-morina-5962497 (1080p).mp4" type="video/mp4" />
+        <video className="background-video" autoPlay playsInline muted loop>
+          <source
+            src="/pexels-george-morina-5962497 (1080p).mp4"
+            type="video/mp4"
+          />
           {/* Provide fallback content for non-supporting browsers */}
         </video>
-        <div className="centered-text">
-          Glen is this what you wanted<span role="img" aria-label="Cool Emoji" className="emoji">😎</span>
+        <div className="content-wrapper">
+          <div className="centered-text">
+            Glen is this what you wanted
+            <span role="img" aria-label="Cool Emoji" className="emoji">
+              😎
+            </span>
+          </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
